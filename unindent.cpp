ebecbee9d1f24f7d6647363e8606ae4454976973
Jonathan Shnayder <rangersps42@gmail.com>
2021-10-21 20:50:55 -0400

    Changed unindent to be 1 file

--- unindent.cpp
+++ unindent.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include "funcs.h"
+#include <cctype>
 
+
+std::string removeLeadingSpaces(std::string line);
 
 int main() {
 	std::string line;
@@ -11,4 +13,22 @@ int main() {
 	}
 
 	return 0;
+}
+
+
+std::string removeLeadingSpaces(std::string line) {
+    bool foundNonSpace = false;
+    std::string result = "";
+    for(int i = 0; i<line.length(); i++) {
+        if(!isspace(line[i]) && !foundNonSpace) {
+            foundNonSpace = true;
+        }
+
+        if(foundNonSpace) {
+            result += line[i];
+
+        }
+
+    }
+    return result;
 }
